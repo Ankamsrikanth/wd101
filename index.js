d0e82c7b805fda3ef9c472b6d894c9c2e80ee6b0
Srikanth Ankam <151608322+Ankamsrikanth@users.noreply.github.com>
2023-11-21 23:43:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,88 +1,99 @@
-const email = document.getElementById('email');
-email.addEventListener('input', () => validate(email));
-
-function validate(element) {
-    if (element.validity.typeMismatch) {
-        element.setCustomValidity("The Email is not in the right format");
-        element.reportValidity();
-    } else {
-        element.setCustomValidity('');
-    }
-}
-
-const userForm = document.getElementById('user-form');
-
-const retrieveEntries = () => {
-    let entries = localStorage.getItem('user-entries');
-    if (entries) {
-        entries = JSON.parse(entries);
-    } else {
-        entries = [];
-    }
-    return entries;
-};
-
-let userEntries = retrieveEntries();
-
-const displayEntries = () => {
-    const entries = retrieveEntries();
-
-    const tableEntries = entries.map((entry) => {
-        const nameCell = `<td class='border px-4 py-2'> ${entry.name} </td>`;
-        const emailCell = `<td class='border px-4 py-2'> ${entry.email} </td>`;
-        const passwordCell = `<td class='border px-4 py-2'> ${entry.password} </td>`;
-        const dobCell = `<td class='border px-4 py-2'> ${entry.dob} </td>`;
-        const acceptTermsCell = `<td class='border px-4 py-2'> ${entry.acceptTermsandcond} </td>`;
-
-        return `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
-    }).join("\n");
-
-    const table = `<table class="table-auto w-full"><tr>
-        <th class="px-4 py-2">Name</th>
-        <th class="px-4 py-2">Email</th>
-        <th class="px-4 py-2">Password</th>
-        <th class="px-4 py-2">DOB</th>
-        <th class="px-4 py-2">Accepted Terms?</th>
-    </tr>${tableEntries}</table>`;
-
-    let details = document.getElementById("user-entries");
-    details.innerHTML = table;
-};
-
-const saveUserForm = (event) => {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const dob = document.getElementById('dob').value;
-    const acceptTermsandcond = document.getElementById('acceptTerms').checked;
-
-    
-    // Calculate age from the provided date of birth
-    const today = new Date();
-    const birthDate = new Date(dob);
-    const age = today.getFullYear() - birthDate.getFullYear();
-
-    // Check if the user's age is between 18 and 55
-    if (age < 18 || age > 55) {
-        alert('Sorry, you must be between 18 and 55 years old to register.');
-        return;
-    }
-    
-    const entry = {
-        name,
-        email,
-        password,
-        dob,
-        acceptTermsandcond,
-    };
-
-    userEntries.push(entry);
-
-    localStorage.setItem('user-entries', JSON.stringify(userEntries));
-    
-    displayEntries();
-};
-
-userForm.addEventListener('submit', saveUserForm);
-displayEntries();
+        <div class="container">
+            <form class="form-horizontal" role="form">
+                <h2>Registration Form</h2>
+                <div class="form-group">
+                    <label for="firstName" class="col-sm-3 control-label">Full Name</label>
+                    <div class="col-sm-9">
+                        <input type="text" id="firstName" placeholder="Full Name" class="form-control" autofocus>
+                        <span class="help-block">Last Name, First Name, eg.: Smith, Harry</span>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-sm-3 control-label">Email</label>
+                    <div class="col-sm-9">
+                        <input type="email" id="email" placeholder="Email" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-sm-3 control-label">Password</label>
+                    <div class="col-sm-9">
+                        <input type="password" id="password" placeholder="Password" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="birthDate" class="col-sm-3 control-label">Date of Birth</label>
+                    <div class="col-sm-9">
+                        <input type="date" id="birthDate" class="form-control">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="country" class="col-sm-3 control-label">Country</label>
+                    <div class="col-sm-9">
+                        <select id="country" class="form-control">
+                            <option>Afghanistan</option>
+                            <option>Bahamas</option>
+                            <option>Cambodia</option>
+                            <option>Denmark</option>
+                            <option>Ecuador</option>
+                            <option>Fiji</option>
+                            <option>Gabon</option>
+                            <option>Haiti</option>
+                        </select>
+                    </div>
+                </div> <!-- /.form-group -->
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Gender</label>
+                    <div class="col-sm-6">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input type="radio" id="femaleRadio" value="Female">Female
+                                </label>
+                            </div>
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input type="radio" id="maleRadio" value="Male">Male
+                                </label>
+                            </div>
+                            <div class="col-sm-4">
+                                <label class="radio-inline">
+                                    <input type="radio" id="uncknownRadio" value="Unknown">Unknown
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- /.form-group -->
+                <div class="form-group">
+                    <label class="control-label col-sm-3">Meal Preference</label>
+                    <div class="col-sm-9">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" id="calorieCheckbox" value="Low calorie">Low calorie
+                            </label>
+                        </div>
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" id="saltCheckbox" value="Low salt">Low salt
+                            </label>
+                        </div>
+                    </div>
+                </div> <!-- /.form-group -->
+                <div class="form-group">
+                    <div class="col-sm-9 col-sm-offset-3">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox">I accept <a href="#">terms</a>
+                            </label>
+                        </div>
+                    </div>
+                </div> <!-- /.form-group -->
+                <div class="form-group">
+                    <div class="col-sm-9 col-sm-offset-3">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                </div>
+            </form> <!-- /form -->
+        </div> <!-- ./container -->
+@Ankamsrikanth
+Comment
+ 
